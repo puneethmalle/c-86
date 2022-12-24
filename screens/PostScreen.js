ccc768db271361c440134e4050669b70b3668e6f
puneethmalle <88337517+puneethmalle@users.noreply.github.com>
2022-12-24 12:34:24 -0600

    Add files via upload

---
+++ screens/PostScreen.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import {View,text,StyleSheet} from 'react-native';
+export default class PostScreen extends Component{
+    render(){
+        return(
+            <View style = {{flex:1,justifyContent:"center",alignItems:"center"}}>
+                <Text>PostScreen</Text>
+            </View>
+        )
+    }
+}
